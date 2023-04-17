ef206ba1440e7662a2163b7045161708bd314792
PrettyMagnolia <109959622+PrettyMagnolia@users.noreply.github.com>
2023-04-17 11:07:48 +0800

    添加场地模块的实体类

---
+++ src/main/java/com/backend/tjtablepartyspringboot/entity/PublicSite.java
@@ -0,0 +1,39 @@
+package com.backend.tjtablepartyspringboot.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * @Author 2051196 刘一飞
+ * @Date 2023/4/17
+ * @JDKVersion 17.0.4
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@TableName(value = "public_site")
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class PublicSite {
+    @JsonSerialize(using= ToStringSerializer.class)
+    Long publicSiteId;
+    @JsonSerialize(using=ToStringSerializer.class)
+    Long creatorId;
+    String name;
+    String city;
+    String location;
+    String picture;
+    String introduction;
+    float avgCost;
+    int capacity;
+    String phone;
+    Date uploadTime;
+    Date checkTime;
+    byte status;
+}
